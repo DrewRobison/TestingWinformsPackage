d5c3c008d902b04b64104d1335903eef2622a3e8
drew.robison <drewrobison@gmail.com>
2025-07-21 07:34:18 -0400

    Add project files.

---
+++ Deployment/Program.cs
@@ -0,0 +1,17 @@
+﻿using static Bullseye.Targets;
+using static SimpleExec.Command;
+
+
+
+Target("build", () => RunAsync("dotnet", "build --configuration Release --nologo --verbosity quiet"));
+Target("test", dependsOn: ["build"], () => RunAsync("dotnet", "test --configuration Release --no-build --nologo --verbosity quiet"));
+Target("pack", dependsOn: ["test"], () => RunAsync("dotnet", "pack --configuration Release --no-build --nologo --verbosity quiet --output C:\\Users\\drewr\\LocalNuget"));
+Target("default", dependsOn: ["test"]);
+
+await RunTargetsAndExitAsync(args, ex => ex is SimpleExec.ExitCodeException);
+
+/*
+ * https://github.com/adamralph/bullseye.git
+ * run from the command line
+ * ex: C:\Users\drewr\source\repos\TestingWinformsPackage>dotnet run --project Deployment
+ */
